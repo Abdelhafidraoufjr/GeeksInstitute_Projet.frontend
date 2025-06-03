f415dbe732bffdbc32b0fa68bbb3cae490548520
BABIOUI-Fatima-Ezzahrae <fatimaezzahrae.babioui@gmail.com>
2025-06-04 00:46:17 +0100

    blogs_santé

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import './components/Navbar'
 import Nav from './components/Navbar'
+import Blogs from './pages/blogs'
 
 export default function App() {
   return (
     <div>
       <Nav />
+      <Blogs />
     </div>
   )
 }
---
+++ src/pages/blogs.jsx
@@ -0,0 +1,198 @@
+import React from 'react';
+
+function Blogs() {
+    return (
+        <div className='p-10 -mt-[7%]'>
+            <div className='flex justify-between'>
+            <div className='mt-40  w-80'>
+                        <article className="overflow-hidden rounded-lg border border-red-100 bg-white shadow-xs">
+                            <img alt="" src="https://www.dergam.com/wp-content/themes/yootheme/cache/e5/comprendre-le-stress-e5ee4761.webp"
+                            className="h-56 w-full object-cover"/>
+
+            <div className="p-4 sm:p-6">
+                <a href="#">
+                <h3 className="text-lg font-medium text-gray-900">
+                    Comprendre les effets du stress sur la santé
+                </h3>
+                </a>
+
+                <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+                Le stress chronique affecte profondément le corps et l’esprit.
+                 Il peut entraîner des troubles du sommeil, une fatigue constante,
+                  une tension musculaire et affaiblir le système immunitaire.
+                </p>
+
+                <a href="#" className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600">
+                Find out more
+
+                <span aria-hidden="true" className="block transition-all group-hover:ms-0.5 rtl:rotate-180">
+                    &rarr;
+                </span>
+                </a>
+            </div>
+            </article>
+        </div>
+        <div className='mt-40  w-80'>
+                        <article className="overflow-hidden rounded-lg border border-red-100 bg-white shadow-xs">
+            <img
+                alt=""
+                src="https://sf.doctissimo.fr/wp-content/uploads/doctissimo/2024/06/16-conseils-pour-eviter-les-problemes-cardiovasculaires-d5f9f81e4c49efbc-646x430.jpeg"
+                className="h-56 w-full object-cover"
+            />
+
+            <div className="p-4 sm:p-6">
+                <a href="#">
+                <h3 className="text-lg font-medium text-gray-900">
+                    Les bons gestes pour un cœur en pleine forme
+                </h3>
+                </a>
+
+                <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+                Un cœur en bonne santé repose sur une alimentation équilibrée, 
+                une activité physique régulière et la gestion du stress.
+                 Il est aussi crucial d’éviter le tabac et surveiller sa tension artérielle.
+                </p>
+
+                <a href="#" className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600">
+                Find out more
+
+                <span aria-hidden="true" className="block transition-all group-hover:ms-0.5 rtl:rotate-180">
+                    &rarr;
+                </span>
+                </a>
+            </div>
+            </article>
+        </div>
+        <div className='mt-40 w-80'>
+                        <article className="overflow-hidden rounded-lg border border-red-100 bg-white shadow-xs">
+            <img
+                alt=""
+                src="https://sf2.doctissimo.fr/wp-content/uploads/doctissimo/2024/06/maladie-pulmonaire-a4e5d124b257237f.jpeg"
+                className="h-56 w-full object-cover"
+            />
+
+            <div className="p-4 sm:p-6">
+                <a href="#">
+                <h3 className="text-lg font-medium text-gray-900">
+                    Les maladies des poumons : symptômes à prendre au sérieux
+                </h3>
+                </a>
+
+                <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+                Une toux persistante, un essoufflement ou des douleurs thoraciques peuvent être les signes de maladies pulmonaires.
+                 Une consultation rapide peut sauver des vies.
+                </p>
+
+                <a href="#" className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600">
+                Find out more
+
+                <span aria-hidden="true" className="block transition-all group-hover:ms-0.5 rtl:rotate-180">
+                    &rarr;
+                </span>
+                </a>
+            </div>
+            </article>
+        </div>
+        </div>
+            <div className='flex justify-between'>
+            <div className='mt-40  w-80'>
+                        <article className="overflow-hidden rounded-lg border border-red-100 bg-white shadow-xs">
+            <img
+                alt=""
+                src="https://img.youm7.com/ArticleImgs/2019/2/4/135357-%D9%85%D8%A7-%D9%87%D9%89-%D8%A7%D8%B3%D8%A8%D8%A7%D8%A8-%D8%A7%D9%84%D8%AA%D9%87%D8%A7%D8%A8-%D8%A7%D9%84%D8%A7%D8%B9%D8%B5%D8%A7%D8%A8.jpg"
+                className="h-56 w-full object-cover"
+            />
+
+            <div className="p-4 sm:p-6">
+                <a href="#">
+                <h3 className="text-lg font-medium text-gray-900">
+                    Neuropathies : comprendre l’inflammation des nerfs
+                </h3>
+                </a>
+
+                <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+                Les inflammations nerveuses provoquent des douleurs, des engourdissements ou une perte de sensibilité.
+                 Elles peuvent être causées par le diabète, des infections ou un traumatisme.
+                </p>
+
+                <a href="#" className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600">
+                Find out more
+
+                <span aria-hidden="true" className="block transition-all group-hover:ms-0.5 rtl:rotate-180">
+                    &rarr;
+                </span>
+                </a>
+            </div>
+            </article>
+        </div>
+        <div className='mt-40  w-80'>
+                        <article className="overflow-hidden rounded-lg border border-red-100 bg-white shadow-xs">
+            <img
+                alt=""
+                src="https://www.aljazeera.net/wp-content/uploads/2023/11/image-648.jpg"
+                className="h-56 w-full object-cover"
+            />
+
+            <div className="p-4 sm:p-6">
+                <a href="#">
+                <h3 className="text-lg font-medium text-gray-900">
+                     Les effets du sucre sur la santé
+                </h3>
+                </a>
+
+                <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+                Une consommation excessive de sucre peut entraîner plusieurs problèmes de santé,
+                 dont le diabète de type 2, l’obésité et les maladies cardiovasculaires.
+                  Le sucre provoque des pics de glycémie qui fatiguent le pancréas et peuvent, sur le long terme, 
+                  perturber la régulation de l’insuline. Il est important de limiter les sucres ajoutés dans 
+                  l’alimentation et de privilégier les aliments naturels pour maintenir une bonne santé.
+                </p>
+
+                <a href="#" className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600">
+                Find out more
+
+                <span aria-hidden="true" className="block transition-all group-hover:ms-0.5 rtl:rotate-180">
+                    &rarr;
+                </span>
+                </a>
+            </div>
+            </article>
+        </div>
+        <div className='mt-40 w-80'>
+                        <article className="overflow-hidden rounded-lg border border-red-100 bg-white shadow-xs">
+            <img
+                alt=""
+                src="https://sommeilenfant.reseau-morphee.fr/wp-content/uploads/sites/5/2018/10/cerveau_sommeil.jpg"
+                className="h-56 w-full object-cover"
+            />
+
+            <div className="p-4 sm:p-6">
+                <a href="#">
+                <h3 className="text-lg font-medium text-gray-900">
+                    Pourquoi bien dormir est essentiel pour la santé
+                </h3>
+                </a>
+
+                <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+                Le sommeil joue un rôle crucial dans la récupération physique et mentale.
+                 Un sommeil de qualité améliore la concentration, renforce le système immunitaire et aide à prévenir
+                  de nombreuses maladies chroniques comme le diabète ou les troubles cardiovasculaires. Adopter une routine de sommeil régulière et éviter les écrans avant de dormir favorise un meilleur repos.
+                </p>
+
+                <a href="#" className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600">
+                Find out more
+
+                <span aria-hidden="true" className="block transition-all group-hover:ms-0.5 rtl:rotate-180">
+                    &rarr;
+                </span>
+                </a>
+            </div>
+            </article>
+        </div>
+        </div>
+        </div>
+       
+    )
+}
+
+export default Blogs;
