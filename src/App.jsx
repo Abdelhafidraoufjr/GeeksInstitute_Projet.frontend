9e2c2ff493f0403e6c87e25c35a515b7ccf40096
adoulServer <adouladoul14@gmail.com>
2025-06-03 21:33:27 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import './App.css'
 import './components/Navbar'
 import Nav from './components/Navbar'
-
+import Home from './pages/home'
 export default function App() {
   return (
     <div>
       <Nav />
+      <Home />
     </div>
   )
 }
