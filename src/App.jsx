bf0f6018c5df7eae76d35053b7b89ef0f743e767
Mooka <47282413+mooka007@users.noreply.github.com>
2025-06-04 01:22:22 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Nav from './components/Navbar';
-import Home from './pages/home';
-import Login from './pages/LoginPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./components/Navbar";
+import Home from "./pages/home";
+import Login from "./pages/LoginPage";
 export default function App() {
   return (
     <Router>
@@ -10,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} /> */}
       </Routes>
     </Router>
   );
